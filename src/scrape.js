03c7affdd6701585f2cb01fb2216575a4f5281ad
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2023-02-27 07:59:00 -0600

    Refactor files

--- src/scrape.js
+++ src/scrape.js
@@ -1,6 +1,5 @@
 // const fetch = require("node-fetch");
 const fetch = require("@adobe/node-fetch-retry");
-const fs = require("fs");
 const { parse } = require("node-html-parser");
 const dayjs = require("dayjs");
 var utc = require("dayjs/plugin/utc"); // dependent on utc plugin
@@ -17,7 +16,7 @@ const {
 } = require("./utils/parse");
 const { ALL_COUNTIES } = require("./constants/counties");
 const { RETRY_OPTIONS } = require("./constants/fetch");
-const { asyncForEach, arrayToCsv } = require("./utils/misc");
+const { asyncForEach } = require("./utils/misc");
 const { schemaFileDts } = require("./utils/validation");
 
 
@@ -104,73 +103,4 @@ const scrape = async (
   return docketData;
 };
 
-const scrapeAndSave = async (
-  {
-    filedStartDate,
-    filedEndDate,
-    counties = "*",
-    outputPath = "."
-  } = {}
-) => {
-  /**
-   * Scrape and upload criminal cases from website of the Administrative Office of Pennsylvania Courts
-   * @param {String} options.filedStartDate Start date of data to scrape in format 'YYYY-MM-DD'. Defaults to yesterday, New York.
-   * @param {String} options.filedEndDate End date of data to scrape in format 'YYYY-MM-DD'. Defaults to filedStartDate
-   * @param {Array, String} options.counties List of counties to get data for. Use "*" for all counties. Defaults to "*"
-   * @param {Array, String} options.outputPath Where to save the file. Defaults to "./"
-   *
-   */
-  // Start
-  const startTime = dayjs().tz("America/New_York");
-  console.log("Scrape begin:", startTime.format("ddd, MMM D, YYYY h:mm A"));
-
-  // set defaults
-  if (!filedStartDate) {
-    filedStartDate = dayjs().subtract(1, "day").format("YYYY-MM-DD");
-  }
-
-  // validation
-  const { error, value } = schemaFileDts.validate({
-    filedStartDate,
-    filedEndDate,
-  });
-  if (error) {
-    throw error;
-  }
-  filedStartDate = value.filedStartDate;
-  filedEndDate = value.filedEndDate;
-  console.log(`Scraping dockets between ${filedStartDate} and ${filedEndDate}...`);
-
-  // scrape
-  const dockets = await scrape(counties, filedStartDate, { filedEndDate });
-  dockets.sort((a, b) =>
-    String(a.primaryParticipants).localeCompare(String(b.primaryParticipants))
-  );
-
-  // write to file
-  if (dockets.length > 0) {
-    // write json
-    const docketJsonPath = `${outputPath}/dockets.json`;
-    const prettyJson = JSON.stringify(dockets, null, 2);
-    fs.writeFileSync(docketJsonPath, prettyJson);
-    console.log(`Saved dockets as JSON to: ${docketJsonPath}`);
-
-    // write csv
-    const docketCsvPath = `${outputPath}/dockets.csv`;
-    const csvStr = arrayToCsv(dockets);
-    fs.writeFileSync(docketCsvPath, csvStr);
-    console.log(`Saved dockets as CSV to: ${docketCsvPath}`);
-
-  } else {
-    console.log("No dockets found for the provided date range.");
-  }
-
-  // scrape analysis
-  const endTime = dayjs().tz("America/New_York");
-  console.log("Scrape end:", endTime.format("ddd, MMM D, YYYY h:mm A"));
-  const scrapeDurationSec = endTime.diff(startTime, "seconds");
-  const scrapeDurationFmt = `${scrapeDurationSec} seconds`;
-  console.log(`Scrape duration: ${scrapeDurationFmt}`);
-};
-
-module.exports = scrapeAndSave
+module.exports = scrape;
